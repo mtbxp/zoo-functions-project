02db2d40003385d28635b1329b54b78e52c25fc7
mtbxp <marcellotb2@gmail.com>
2022-06-03 04:14:38 -0300

    tests handlerElephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,25 @@
+const { TestScheduler } = require('@jest/core');
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('retorna a quantidade de elefantes', () => {
+      expect(handlerElephants('count')).toBe(4);
+  });
+  test('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(handlerElephants('names')).toEqual(expected);
+  });
+  test('retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  test('retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  test('retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+  test('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(handlerElephants('availability')).toEqual(expected);
+  });
+});
